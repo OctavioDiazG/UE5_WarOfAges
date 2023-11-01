df3dad6a13f0aa194772ae591c1fd5aca8bcd95c
OctavioDiazG <odiazg01@outlook.com>
2023-11-01 10:31:31 -0600

    Start Project

---
+++ Source/WarOfAges/WarOfAgesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarOfAgesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAROFAGES_API AWarOfAgesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
